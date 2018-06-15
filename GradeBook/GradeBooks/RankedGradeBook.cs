9e9da7ff8202ecd3374f07b651a9f32835349cb1
Andrew Crouse <andrew.crouse@gmail.com>
2018-06-14 22:40:17 -0400

    Make Ranked and Standard Gradebooks public
    Make RankedGradeBook and Standard Gradebook public.

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace GradeBook.GradeBooks 
 {
-    class RankedGradeBook : BaseGradeBook
+    public class RankedGradeBook : BaseGradeBook
     {
         public RankedGradeBook(string name) :base(name)
         {
